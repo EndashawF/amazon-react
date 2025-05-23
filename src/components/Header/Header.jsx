efc2959855cbef8621bf08c6daeec90fd181d785
EndashawF <endashawdabi@gmail.com>
2025-05-23 01:20:29 -0400

    Add to Cart functionality

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import classes from "./Header.module.css";
 import {Link} from "react-router-dom"
 import { SlLocationPin } from "react-icons/sl";
 import { BsSearch } from "react-icons/bs";
 import LowerHeader from './LowerHeader';
 import { BiCart } from "react-icons/bi";
+import { DataContext } from '../DataProvider/DataProvider';
 
 function Header() {
+
+  const [{basket}, dispatch]=useContext(DataContext)
+  console.log(basket)
   return (
     <>
       <section>
@@ -29,51 +33,45 @@ function Header() {
               </div>
             </div>
           </div>
-          
-            {/* search section*/}
-            <div className={classes.search}>
+
+          {/* search section*/}
+          <div className={classes.search}>
+            <select name="" id="">
+              <option value="">All</option>
+            </select>
+            <input type="text" />
+            <BsSearch size={25} />
+          </div>
+          {/* other section */}
+          <div className={classes.order_container}>
+            <Link to="" className={classes.language}>
+              <img
+                src="https://media.istockphoto.com/id/1420904901/vector/specification-perfect-correct-dimension-official-american-flag-red-white-blue-stripes.jpg?s=1024x1024&w=is&k=20&c=LDGu9UTa-LmTZbK-sAvr5a_G4UC043w2HKcpAP9nr-Q="
+                alt=""
+              />
               <select name="" id="">
-                <option value="">All</option>
+                <option value="">EN</option>
               </select>
-              <input type="text" />
-              <BsSearch size={25} />
-            </div>
-            {/* other section */}
-            <div className= {classes.order_container}>
-              
-                <Link to="" className={classes.language}>
-                  <img
-                    src="https://media.istockphoto.com/id/1420904901/vector/specification-perfect-correct-dimension-official-american-flag-red-white-blue-stripes.jpg?s=1024x1024&w=is&k=20&c=LDGu9UTa-LmTZbK-sAvr5a_G4UC043w2HKcpAP9nr-Q="
-                    alt=""
-                  />
-                  <select name='' id=''>
-                    <option value="">EN</option>
-                  </select>
-                </Link>
-             
-             
-              <Link to="">
-                
-                  <p>Sign In</p>
-                  <span>Account & Lists</span>
-                
-              </Link>
-              
-              <Link to="/orders">
-                <p>returns</p>
-                <span>& Orders</span>
-              </Link>
-             
-              <Link to='/cart' className={classes.cart}>
-                <BiCart size ={40}/>
-                <span>0</span>
-              </Link>
-            </div>
-          
-         
+            </Link>
+
+            <Link to="">
+              <p>Sign In</p>
+              <span>Account & Lists</span>
+            </Link>
+
+            <Link to="/orders">
+              <p>returns</p>
+              <span>& Orders</span>
+            </Link>
+
+            <Link to="/cart" className={classes.cart}>
+              <BiCart size={50} />
+              <span>{basket?.length || 0}</span>
+            </Link>
+          </div>
         </div>
       </section>
-      <LowerHeader/>
+      <LowerHeader />
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,37 @@
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
 import "./index.css";
 import App from "./App.jsx";
+import { DataProvider } from "./components/DataProvider/DataProvider.jsx";
+import {initialState, reducer} from "../src/components/Utility/reducer.jsx"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <DataProvider reducer={reducer} initialState={initialState}>
+        <App />
+      </DataProvider>
     </BrowserRouter>
   </StrictMode>
 );
+
+
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import { BrowserRouter } from "react-router-dom";
+// import "./index.css";
+// import App from "./App.jsx";
+// import { DataProvider } from "./components/DataProvider/DataProvider.jsx";
+// import {initialState, reducer} from './Utility/reducer.jsx'
+
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <BrowserRouter>
+//       <DataProvider reducer={reducer} initialState={initialState}>
+//         <App />
+//       </DataProvider>
+//     </BrowserRouter>
+//   </StrictMode>
+// );
